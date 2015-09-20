aca95d789f8f5dc6df8da3f8b5bf3d8f65f9fc16
Gyuri Horak <dyuri@horak.hu>
2015-09-20 19:04:14 +0200

    zprezto update & tmux plugins

--- tn.sh
+++ tn.sh
@@ -9,20 +9,22 @@ echo "{\"images\":[" > images.json
 
 # images
 for i in *.jpg; do 
-    nice -n 10 jpegrotate $i
-    nice -n 10 convert $i -resize 200 -quality 70 .thumb/tn_$i
-    nice -n 10 convert $i -resize 1000 -quality 25 .thumb/qv_$i
-    echo "{\"image\": \"$i\", \"thumbnail\": \".thumb/tn_$i\", \"quickview\": \".thumb/qv_$i\"}," >> images.json
-    echo $i done...
+  nice -n 10 jpegrotate $i
+  nice -n 10 convert $i -resize 200 -quality 70 .thumb/tn_$i
+  nice -n 10 convert $i -resize 1000 -quality 25 .thumb/qv_$i
+  echo "{\"image\": \"$i\", \"thumbnail\": \".thumb/tn_$i\", \"quickview\": \".thumb/qv_$i\"}," >> images.json
+  echo $i done...
 done
 
 # videos
 for i in *.avi *.mp4; do 
+  if [ -f $i ]; then
     nice -n 10 ffmpeg -y -i $i -vframes 1 -f image2 .thumb/vid_$i.jpg
     nice -n 10 convert .thumb/vid_$i.jpg -resize 200 -quality 70 .thumb/tn_$i.jpg
     nice -n 10 convert .thumb/vid_$i.jpg -resize 1000 -quality 25 .thumb/qv_$i.jpg
     echo "{\"name\": \"$i\", \"image\": \".thumb/vid_$i.jpg\", \"thumbnail\": \".thumb/tn_$i.jpg\", \"quickview\": \".thumb/qv_$i.jpg\", \"type\": \"video\", \"file\": \"$i\"}," >> images.json
     echo $i done...
+  fi
 done
 
 echo "{}]}" >> images.json
