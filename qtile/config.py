585ab10b683385b6f24ec516ce844ff0407f0014
Gyuri Horak <dyuri@horak.hu>
2020-10-25 16:37:00 +0100

    qtile: swallow

--- qtile/config.py
+++ qtile/config.py
@@ -7,6 +7,7 @@ from plasma import Plasma
 from qtools.xresources import get as get_resources
 import qtools.focus
 import os
+import psutil
 
 COLORS = {
     "color0": "#282828",
@@ -450,6 +451,28 @@ def restart_on_randr(ev):
         _first_screen_change = False
     else:
         qtile.cmd_restart()
+
+
+@hook.subscribe.client_new
+def _swallow(window):
+    pid = window.window.get_net_wm_pid()
+    ppid = psutil.Process(pid).ppid()
+    cpids = {c.window.get_net_wm_pid(): wid for wid, c in window.qtile.windows_map.items()}
+    for i in range(5):
+        if not ppid:
+            return
+        if ppid in cpids:
+            parent = window.qtile.windows_map.get(cpids[ppid])
+            parent.minimized = True
+            window.parent = parent
+            return
+        ppid = psutil.Process(ppid).ppid()
+
+
+@hook.subscribe.client_killed
+def _unswallow(window):
+    if hasattr(window, 'parent'):
+        window.parent.minimized = False
 
 
 _first_screen_change = True
