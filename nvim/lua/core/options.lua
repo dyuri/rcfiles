3373c4d75b2d5b1741b313c86c647e9bb41d5e61
Gyuri Horak <dyuri@horak.hu>
2025-02-12 12:43:30 +0100

    osc copy for nvim

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -43,6 +43,16 @@ vim.o.timeoutlen = 300
 -- Clipboard
 vim.o.clipboard = "unnamedplus"
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+    local copy_to_unnamedplus = require("vim.ui.clipboard.osc52").copy("+")
+    copy_to_unnamedplus(vim.v.event.regcontents)
+    local copy_to_unnamed = require("vim.ui.clipboard.osc52").copy("*")
+    copy_to_unnamed(vim.v.event.regcontents)
+  end,
+})
+
 -- Backspace key
 vim.o.backspace = "indent,eol,start"
 
